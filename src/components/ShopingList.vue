c831cf63cd093ea6cad10e7c27aced1f10312b1b
grzejatol <grzejatol@gmail.com>
2023-10-01 19:28:25 +0100

    Initial commit

---
+++ src/components/ShopingList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<main>
+  <h2>ShopingList</h2>
+
+
+</main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Shoping List</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Shoping List</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ShopingList name="Shoping List" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ShopingList from "@/components/ShopingList.vue";
+</script>
